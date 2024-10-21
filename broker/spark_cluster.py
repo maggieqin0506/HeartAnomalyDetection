f81afb53046692f15a57442a0439f5e3a8095ce5
Dung Dong <dongtandung2001@gmail.com>
2024-10-20 22:50:09 -0700

    added offset for spark streaming, rename to spark_cluster

--- broker/spark_cluster.py
+++ broker/spark_cluster.py
@@ -4,19 +4,23 @@ spark = SparkSession.builder \
     .appName("KafkaStreamProcessing") \
     .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.3") \
     .config("spark.driver.extraJavaOptions", "-Djava.security.manager=allow") \
-    .config("spark.executor.extraJavaOptions", "-Djava.security.manager=allow").getOrCreate()
+    .config("spark.executor.extraJavaOptions", "-Djava.security.manager=allow") \
+    .getOrCreate()
 
 kafka_df = spark.readStream.format("kafka") \
-.option("kafka.bootstrap.servers", "localhost:9092") \
-.option("subscribe", "heartbeat") \
-.load()
+    .option("startingOffsets", "earliest") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "heartbeat") \
+    .option("kafka.group.id", "spark_cluster") \
+    .load()
 
 kafka_values = kafka_df.selectExpr("CAST(value AS STRING)")
 
-# Process the data
+# Process the data with checkpointing
 query = kafka_values.writeStream \
   .outputMode("append") \
   .format("console") \
+  .option("checkpointLocation", "/tmp/checkpoint") \
   .start()
 
 query.awaitTermination()
