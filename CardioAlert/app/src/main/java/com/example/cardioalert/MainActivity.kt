2e3ea45fadbf846b1db050bb27aa4bfa0dbd8206
Maggie Qin <maggie.qin0506@gmail.com>
2024-10-21 10:23:15 -0700

    Update MainActivity.kt

--- CardioAlert/app/src/main/java/com/example/cardioalert/MainActivity.kt
+++ CardioAlert/app/src/main/java/com/example/cardioalert/MainActivity.kt
@@ -13,6 +13,7 @@ import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.IMqttActionListener
 import org.eclipse.paho.client.mqttv3.IMqttToken
 import org.eclipse.paho.client.mqttv3.MqttClient
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
 import org.eclipse.paho.client.mqttv3.MqttException
 import org.eclipse.paho.client.mqttv3.MqttMessage
 
